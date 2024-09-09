e7a7be7bc0d713dfc7a03f1101a692041c71f2ad
Mohammad Osoolian <linser@localhost.localdomain>
2024-09-09 03:30:12 +0330

    [feat]: enable wsgi

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def hello():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create tables if they don't exist
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
